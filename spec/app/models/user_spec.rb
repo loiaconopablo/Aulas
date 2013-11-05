90c3bf59f51bb1141f41b310f36db1cff701dede
Pablo Loiacono <loiaconopablo@gmail.com>
2013-11-05 13:31:14 -0300

    Borrado de archivos fetaures, spec y steps. Tambien se corrigio lo de travis que dijo Nico

--- spec/app/models/user_spec.rb
+++ spec/app/models/user_spec.rb
@@ -14,68 +14,68 @@ describe User do
 
 	end
 
-	describe 'valid?' do
+	# describe 'valid?' do
 
-	  let(:user) { User.new }
+	#   let(:user) { User.new }
 
-	  it 'should be false when name is blank' do
-	  	user.email = 'john.doe@someplace.com'
-	  	user.password = 'a_secure_passWord!'
-	  	user.valid?.should be_false
-	  end
+	#   it 'should be false when name is blank' do
+	#   	user.email = 'john.doe@someplace.com'
+	#   	user.password = 'a_secure_passWord!'
+	#   	user.valid?.should be_false
+	#   end
 
 
-	  it 'should be false when email is not valid' do
-	  	user.name = 'John Doe'
-	  	user.email = 'john'
-	  	user.password = 'a_secure_passWord!'
-	  	user.valid?.should be_false
-	  end
+	#   it 'should be false when email is not valid' do
+	#   	user.name = 'John Doe'
+	#   	user.email = 'john'
+	#   	user.password = 'a_secure_passWord!'
+	#   	user.valid?.should be_false
+	#   end
 
-	  it 'should be false when password is blank' do
-	  	user.name = 'John Doe'
-	  	user.email = 'john.doe@someplace.com'
-	  	user.valid?.should be_false
-	  end
+	#   it 'should be false when password is blank' do
+	#   	user.name = 'John Doe'
+	#   	user.email = 'john.doe@someplace.com'
+	#   	user.valid?.should be_false
+	#   end
 
-	  it 'should be true when all field are valid' do
-	  	user.name = 'John Doe'
-	  	user.email = 'john.doe@someplace.com'
-	  	user.password = 'a_secure_passWord!'
-	  	user.valid?.should be_true
-	  end
+	#   it 'should be true when all field are valid' do
+	#   	user.name = 'John Doe'
+	#   	user.email = 'john.doe@someplace.com'
+	#   	user.password = 'a_secure_passWord!'
+	#   	user.valid?.should be_true
+	#   end
 
-	end
+	# end
 
-	describe 'authenticate' do
+	# describe 'authenticate' do
 
-		before do
-			@password = 'password'
-		 	@user = User.new
-		 	@user.email = 'john.doe@someplace.com'
-		 	@user.password = @password
-		end
+	# 	before do
+	# 		@password = 'password'
+	# 	 	@user = User.new
+	# 	 	@user.email = 'john.doe@someplace.com'
+	# 	 	@user.password = @password
+	# 	end
 
-		it 'should return nil when password do not match' do
-			email = @user.email
-			password = 'wrong_password'
-			User.should_receive(:find_by_email).with(email).and_return(@user)
-			User.authenticate(email, password).should be_nil
-		end
+	# 	it 'should return nil when password do not match' do
+	# 		email = @user.email
+	# 		password = 'wrong_password'
+	# 		User.should_receive(:find_by_email).with(email).and_return(@user)
+	# 		User.authenticate(email, password).should be_nil
+	# 	end
 
-		it 'should return nil when email do not match' do
-			email = 'wrong@email.com'
-			User.should_receive(:find_by_email).with(email).and_return(nil)
-			User.authenticate(email, @password).should be_nil
-		end
+	# 	it 'should return nil when email do not match' do
+	# 		email = 'wrong@email.com'
+	# 		User.should_receive(:find_by_email).with(email).and_return(nil)
+	# 		User.authenticate(email, @password).should be_nil
+	# 	end
 
-		it 'should return the user when email and password match' do
-			email = @user.email
-			User.should_receive(:find_by_email).with(email).and_return(@user)
-			User.authenticate(email, @password).should eq @user
-		end
+	# 	it 'should return the user when email and password match' do
+	# 		email = @user.email
+	# 		User.should_receive(:find_by_email).with(email).and_return(@user)
+	# 		User.authenticate(email, @password).should eq @user
+	# 	end
 
-	end
+	# end
 
 end
 
