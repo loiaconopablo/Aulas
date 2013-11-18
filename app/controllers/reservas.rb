feff8ac9bb54790e7a72b197cb22a38c8d9f5e28
Pablo Loiacono <loiaconopablo@gmail.com>
2013-11-18 15:50:32 -0300

    Se avanzo con la storie de Aceptar Reserva

--- app/controllers/reservas.rb
+++ app/controllers/reservas.rb
@@ -1,12 +1,19 @@
 Aulas::App.controllers :reservas do
 
   get :listar do
-    @reservas = Reserva.all
-    render 'reservas/listado'
+  	@reservas = Reserva.all
+  	render 'reservas/listado'
   end
   
   get :validate do
+  	@reserva = Reserva.get(params[:reserva_id])
+  	@aula = @reserva.aula
+  	@docente = User.get(@reserva.user_id)
+  	@aulaAModificar = Aula.get(@aula.id)
+  	@obse = "Se reservo a nombre de "+@docente.name+" para la materia "+@reserva.materia
+  	@aulaAModificar.update(:estado => true, :observaciones => @obse)
+  	@reserva.destroy
+  	redirect 'reservas/listar'
+  end
 
-  end  
-  
 end
